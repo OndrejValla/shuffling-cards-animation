f4ca586580243ad11a1081e18eeb4385effbfba8
OndrejValla <ondrej.valla@gmail.com>
2022-01-13 19:49:03 +0000

    Created playing cards, displayed each suite row by using JavaScript

---
+++ script.js
@@ -0,0 +1,62 @@
+const numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+const suits = [{
+    icon: '♥️',
+    color: 'red',
+}, {
+    icon: '♣️',
+    color: 'black',
+}, {
+    icon: '♦️',
+    color: 'red',
+}, {
+    icon: '♠️',
+    color: 'black',
+}, ];
+
+const container = document.getElementById('container');
+
+function createCard({
+    number,
+    suit,
+    suit_idx,
+    number_idx
+}) {
+    const cardEl = document.createElement('div');
+
+    cardEl.classList.add('card');
+
+    if (suit.color === 'red') {
+        cardEl.classList.add('red');
+    }
+
+    cardEl.style.top = suit_idx * 175 + 'px';
+    cardEl.style.left = number_idx * 120 + 'px';
+
+    cardEl.innerHTML = `
+        <span class="number top">
+            ${number}
+        </span>
+        <p class="suit">
+            ${suit.icon}
+        </p>
+        <span class="number bottom">
+            ${number}
+        </span>
+    `;
+
+    container.appendChild(cardEl);
+}
+
+// Creating cards
+suits.forEach((suit, suit_idx) => {
+    numbers.forEach((number, number_idx) => {
+        const cardDetails = {
+            number,
+            suit,
+            suit_idx,
+            number_idx
+        }
+        createCard(cardDetails);
+    });
+});
